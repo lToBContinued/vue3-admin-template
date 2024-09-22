0e1c69d628ae6cb9f04c1c9b2c4135173a648918
wutong <wt3212834434@outlook.com>
2024-09-22 11:21:20 +0800

    feat:用户管理界面

--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -1,9 +1,57 @@
 <template>
-  <div>
-    <h1>用户管理</h1>
+  <div class="userLayout">
+    <el-card>
+      <el-form ref="form" v-model="formData" :inline="true">
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="formData.userName" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="search">搜索</el-button>
+          <el-button @click="reset">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+
+    <el-card>
+      <el-button type="primary">添加用户</el-button>
+      <el-button type="danger">批量删除</el-button>
+      <!--用户信息-->
+      <el-table :data="userList" style="margin: 10px 0" border>
+        <el-table-column type="selection" width="55" align="center"></el-table-column>
+        <el-table-column label="#" align="center"></el-table-column>
+        <el-table-column label="id" align="center"></el-table-column>
+        <el-table-column label="用户名" align="center"></el-table-column>
+        <el-table-column label="昵称" align="center"></el-table-column>
+        <el-table-column label="用户角色" align="center"></el-table-column>
+        <el-table-column label="创建时间" align="center"></el-table-column>
+        <el-table-column label="更新时间" align="center"></el-table-column>
+        <el-table-column label="操作" width="260" align="center"></el-table-column>
+      </el-table>
+      <gn-pagination :current-page="pageNo" :page-size="pageSize" :total="0"></gn-pagination>
+    </el-card>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
 
-<style scoped lang="scss"></style>
+const pageNo = 1
+const pageSize =5
+// 表单数据
+const formData = ref({
+  userName: ''
+})
+const form = ref(null) // 表单实例
+const userList = ref([]) // 用户数据列表
+
+// 搜索
+const search = () => {}
+// 重置
+const reset = () => {}
+</script>
+
+<style scoped lang="scss">
+.el-form-item {
+  margin-bottom: 0;
+}
+</style>

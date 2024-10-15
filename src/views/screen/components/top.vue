bcdf8e07fd35b51b6b5e091836fbdd7ffd97c911
wutong <wt3212834434@outlook.com>
2024-10-15 16:33:09 +0800

    feat:数据大屏顶部

---
+++ src/views/screen/components/top.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="top">
+    <div class="left">
+      <span class="lbtn" @click="goHome">首页</span>
+    </div>
+    <div class="center">
+      <div class="title">智慧旅游可视化大数据平台</div>
+    </div>
+    <div class="right">
+      <span class="rbtn">统计报告</span>
+      <span class="time">当前时间：{{ time }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
+import moment from 'moment'
+
+const $router = useRouter()
+const time = ref(moment().format('YYYY-MM-DD hh:mm:ss')) // 当前时间
+const timer = ref(null) // 定时器
+
+onMounted(() => {
+  // 更新时间
+  setInterval(() => {
+    time.value = moment().format('YYYY-MM-DD HH:mm:ss')
+  }, 1000)
+})
+onBeforeUnmount(() => {
+  timer.value = null // 清除定时器
+})
+
+// 点击首页按钮回到首页
+const goHome = () => {
+  $router.push('/home')
+}
+</script>
+
+<style lang="scss" scoped>
+.top {
+  display: flex;
+  width: 100%;
+  height: 40px;
+}
+
+.left {
+  flex: 1.5;
+  background: url('../images/dataScreen-header-left-bg.png') no-repeat;
+  background-size: cover;
+
+  .lbtn {
+    float: right;
+    width: 150px;
+    height: 40px;
+    background: url('../images/dataScreen-header-btn-bg-l.png') no-repeat;
+    background-size: cover;
+    text-align: center;
+    line-height: 40px;
+    color: #19fffc;
+    font-size: 18px;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+
+.center {
+  flex: 2;
+
+  .title {
+    width: 100%;
+    height: 74px;
+    background: url('../images/dataScreen-header-center-bg.png') no-repeat;
+    background-size: 100% 100%;
+    text-align: center;
+    line-height: 74px;
+    color: #19fffc;
+    font-size: 30px;
+  }
+}
+
+.right {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex: 1.5;
+  background: url('../images/dataScreen-header-left-bg.png') no-repeat;
+  background-size: cover;
+
+  .rbtn {
+    width: 150px;
+    height: 40px;
+    background: url('../images/dataScreen-header-btn-bg-r.png') no-repeat;
+    background-size: 100% 100%;
+    text-align: center;
+    line-height: 40px;
+    color: #19fffc;
+    font-size: 18px;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  .time {
+    margin-right: 40px;
+    color: #19fffc;
+    font-size: 20px;
+  }
+}
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,7 +1,77 @@
+<!--suppress JSUnresolvedReference -->
 <template>
-  <div><h1>数据大屏一级路由组件</h1></div>
+  <div class="screenLayout">
+    <!--展示内容区域-->
+    <div class="screen" ref="screen">
+      <div class="top">
+        <top></top>
+      </div>
+      <div class="bottom">
+        <div class="left">左侧</div>
+        <div class="center">中间</div>
+        <div class="right">右侧</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+import Top from '@/views/screen/components/top.vue'
 
-<style scoped lang="scss"></style>
+const screen = ref(null) // 数据大屏内容的dom
+
+onMounted(() => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`
+})
+
+// 获取缩放比例
+const getScale = (w = 1920, h = 1080) => {
+  const ww = window.innerWidth / w
+  const wh = window.innerHeight / h
+  return Math.min(ww, wh)
+}
+// 监听视口变化
+window.onresize = () => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`
+}
+</script>
+
+<style scoped lang="scss">
+.screenLayout {
+  width: 100vw;
+  height: 100vh;
+  background: url('./images/bg.png') no-repeat;
+  background-size: cover;
+}
+
+.screen {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  width: 1920px;
+  height: 1080px;
+  transform-origin: left top;
+}
+
+.top {
+  width: 100%;
+  height: 40px;
+}
+
+.bottom {
+  display: flex;
+
+  .right {
+    flex: 1;
+  }
+
+  .left {
+    flex: 1;
+  }
+
+  .center {
+    flex: 2;
+  }
+}
+</style>
